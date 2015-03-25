d878c1ab06aa350348a221eb6551b7256373b1fd
Lab103-10 <Lab103-10@Lab103-10-PC>
2015-03-25 11:52:39 -0300

    Criação do arquivo principal de trabalho , Edson, Almir, Alberto

---
+++ Trab_equipe/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <html>
+<body>
+
+<h1><b>Este é o título Principal</b></h1>
+<h2>  Este é  o título secundário</h2>        
+<h3 > Estou adorando criar páginas    </h3>  
+<br>
+<hr>    
+<p>Este é o 1°Primeiro Paragrafo<p>
+<p>Esta é a 2ªLinha do 1° Paragrafo<p>
+<br>
+<p>Com este curso inicio um paragráfo<p>
+<p>E Com este curso quebro uma linha<p>
+<hr>
+<br><br>
+<b>Texto em Negrito</b><br>
+<i> Texto em Itálico </i><br>
+<u>Texto sublinhado</u>
+<br><br>
+<font face="arial"  color="red">Texto em Vermelho</font>
+<br>
+<font face="arial"   size="5" >Texto em tamanho 5 </font>
+<br>
+<font face="verfana"  size="4" > Texto com letra verdana</font>
+<br>
+<font face="arial"         size="3" >Voce pode fazer combinações</font>
+
+</body>
+</html>
+    </body>
+</html>
